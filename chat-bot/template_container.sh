9e9d12cc9b689f6f93cad6326273d0150635973b
Sadao Hiratsuka <sh2@pop01.odn.ne.jp>
2023-12-31 13:48:25 +0900

    Add Dockerfile, requirements.txt, and template_container.sh for chat bot deployment

---
+++ chat-bot/template_container.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+podman run \
+        --detach \
+        --restart=always \
+        --env=http_proxy= \
+        --env=https_proxy= \
+        --env=no_proxy= \
+        --env=MATTERMOST_URL=http://127.0.0.1 \
+        --env=MATTERMOST_PORT=8065 \
+        --env=MATTERMOST_API_PATH=/api/v4 \
+        --env=BOT_TOKEN= \
+        --env=GEMINI_MODEL=gemini-pro \
+        --env=GEMINI_API_KEY= \
+        --name=gemini-chat-bot \
+        gemini-chat-bot:latest
